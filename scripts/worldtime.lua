025ef58ee1d03de2d934aa67fe5889e402535a75
Aaron Zauner <azet@azet.org>
2014-11-04 03:52:18 +0100

    add script directory and aliases

---
+++ scripts/worldtime.lua
@@ -0,0 +1,17 @@
+#!/usr/bin/env luajit
+-- print current time in different places
+local fmt       = "\t %X | %A, %d %B %Y"
+local now       = os.time()
+local hour      = 3600
+local timezones = {
+  -1, -2, -3, -5, -7, -9,
+   1,  2,  3,  5,  7,  9,
+   0
+}
+local tz_time = function(time)
+  return os.date(fmt, time)
+end
+
+for i, e in ipairs(timezones) do
+  print(e .. tz_time(now + e * hour))
+end
